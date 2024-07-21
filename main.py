85030aad154bdb257c4099cf05e9ed73c2203f95
unknown <vani.08.16.22@gmail.comgit config --global user.name yoko-s89>
2024-07-21 21:25:39 +0900

    初めてのコミット

---
+++ main.py
@@ -0,0 +1,53 @@
+import sys
+
+print(sys.path)
+# sys.path.append('D:\\')
+# print(sys.path)
+
+# import sub
+# sub.sample_a()
+# class_a = sub.ClassA()
+# class_a.print_a()
+# print(sub.VAR)
+
+
+# from sub import sample_a as sa, ClassA as ca
+
+# sa()
+
+# ins = ca()
+# ins.print_a()
+# from sub import *
+
+# print(VAR)
+# sample_a()
+
+# import dir1.base1 as base1
+
+# # dir1.base1.print_msg()
+# # base1.print_msg()
+# import dir1.base2 as base2
+# # base2.print_msg()
+
+
+# from dir1.base1 import print_msg as base1_print
+# from dir1.base2 import print_msg as base2_print
+
+# base1_print()
+# base2_print()
+
+# __init__
+
+
+# from dir1 import *
+
+# base1.print_msg()
+# base2.print_msg()
+# from dir1 import base1_msg
+
+# base1_msg()
+# from dir1 import *
+# from dir1 import base1_msg
+# base1_msg()
+# base2_msg()
+
